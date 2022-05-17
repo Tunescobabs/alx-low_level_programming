397e77e6f1a6e469b841db813d45fbbeee461243
Babatunde Olaniyi <olaniyibabatunde38@gmail.com>
2022-05-16 22:47:31 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,54 +1,31 @@
 /* Author: Babatunde Olaniyi */
 
 
-Izimartin
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-alx-low_level_programming/0x10-variadic_functions/1-print_numbers.c
-@Izimartin
-Izimartin 0x10-variadic_functions
- 1 contributor
-34 lines (27 sloc)  679 Bytes
-/*
- * File: 1-print_numbers.c
- * Auth: Tolulope Fakunle
- */
-
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
- * @...: A variable number of numbers to be printed.
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index;
+	unsigned int i;
+	va_list list;
 
-	va_start(nums, n);
+	va_start(list, n);
 
-	for (index = 0; index < n; index++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, int));
-
-		if (index != (n - 1) && separator != NULL)
-			printf("%s", separator);
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
-	printf("\n");
+	va_end(list);
 
-	va_end(nums);
+	printf("\n");
 }
