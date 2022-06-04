523779a86ba091c2033f6085821c0f2f1b84c04e
Babatunde Olaniyi <olaniyibabatunde38@gmail.com>
2022-06-04 06:01:40 -0700

    Changed main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-
 int create_file(const char *filename, char *text_content);
-
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* __MAIN_H__ */
